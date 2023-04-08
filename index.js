df77de9a2d526621e58ce0418e59e2e1a4f75a16
Avery Maxwell <averymaxwell42@gmail.com>
2023-04-08 13:15:26 -0500

    Add mouseover event listener to grid and timeout

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ for (i = 0; i < 256; i++) {
     let div = document.createElement('div');
     div.classList.add('square');
     mainDiv.appendChild(div);
-}
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'blue';
+
+        setTimeout(() => {
+            e.target.style.backgroundColor = '';
+        }, 1000);
+    })
+};
+
+let btn = document.createElement('button');
+
+document.querySelector('body').insertBefore(btn, mainDiv);
+
+// btn.innerHTML('Click to update number of squares');
+
+// function updateGrid(numberOfSquares) {
+//     let numberOfSquares = prompt('Enter the number of squares per side (100 max):');
+// };
+
+btn.addEventListener('click', updateGrid());
 
 // let div = document.createElement('div');
 // div.classList.add('square');
@@ -17,18 +36,3 @@ for (i = 0; i < 256; i++) {
 // mainDiv.appendChild(gridArray[1]);
 // mainDiv.appendChild(gridArray[2]);
 // console.log(gridArray[1]);
-
-// function createDivs(numberOfDivs) {
-
-//     for (i = 0; i < numberOfDivs; i++) {
-        
-
-//         gridArray.push('div')
-//     }
-// }
-
-// function appendDivs() {
-//     createDivs(16);
-
-
-// }
