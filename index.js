eb4357aaf58b27e64da14f902db52a4bbffcd8f9
Avery Maxwell <averymaxwell42@gmail.com>
2023-05-16 21:53:13 -0500

    Update loop that creates divs - Create two variables that pull in the square class and sets the left and top border for each child div - Calculate square width by dividing the number of squares from container width and subtracting the border width - Set child divs width and heigh to result from calculation - Add a mouseover event listener that sets the each child div color to blue, then removes color after 1000ms
    Add a click event listener to the button
    - Prompt user to enter the root of the number of squares in the grid
    - Create if statement that checks if the string is not a number and
    converts it to an integer if it is a number
    - Create a second if statement that checks if the integer is a whole
    number, if it is above 2 and below 1, and if it passes the tests,
    removes the current child divs and calls createNewGrid function with
    the integer as an input
    
    Add createNewGrid function
    - Copy and paste for loop that creates grid in createNewGrid function
    - Update for loop so that it runs the the same number of times as the
    user input to the power of 2
    - Update squaresWidth calculation so it divides by the user input

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 
+// Set mainDiv variable to conatiner
+
 let mainDiv = document.getElementById('conatiner-div');
+
+// Create button, insert it before the container-div, and add text
+
+let btn = document.createElement('button');
+
+document.querySelector('body').insertBefore(btn, mainDiv);
+
+btn.textContent = 'Click to update number of squares';
+
+// Create initial 16 by 16 Etch-A-Sketch grid
 
 for (i = 0; i < 256; i++) {
     let div = document.createElement('div');
     div.classList.add('square');
+
+    let squareBorderLeft = document.styleSheets[0].cssRules[0].style;
+    squareBorderLeft.border = '.1px solid black';
+
+    let squareBorderTop = document.styleSheets[0].cssRules[0].style;
+    squareBorderTop.border = '.1px solid black';
+
+    let squaresWidth = (720 / 16) - 2;
+
+    let squareClassWidth = document.styleSheets[0].cssRules[0].style;
+    squareClassWidth.setProperty('width', `${squaresWidth}px`);
+
+    let squareClassHeight = document.styleSheets[0].cssRules[0].style;
+    squareClassHeight.setProperty('height', `${squaresWidth}px`);
+
     mainDiv.appendChild(div);
     div.addEventListener('mouseover', (e) => {
         e.target.style.backgroundColor = 'blue';
@@ -14,17 +41,56 @@ for (i = 0; i < 256; i++) {
     })
 };
 
-let btn = document.createElement('button');
+btn.addEventListener('click', (e) => {
+    let numberOfSquares = prompt('Enter the number of squares per side (100 max):');
 
-document.querySelector('body').insertBefore(btn, mainDiv);
+    if (isNaN(numberOfSquares)) {
+        alert('You did not enter a number. Try again.');
+    } else {
+        numberOfSquares = parseInt(numberOfSquares);
+    }
+    
+    if (Number.isInteger(numberOfSquares) == 'false')  {
+        alert('You did not enter a whole number. Try again.');
+    } else if ((numberOfSquares > 100) || (numberOfSquares < 2)) {
+        alert('You entered a number more than 100 or less than 2. Try again.');
+    } else {
+        mainDiv.innerHTML = '';
+        console.log(numberOfSquares);
+        createNewGrid(numberOfSquares);
+    };
+});
 
-// btn.innerHTML('Click to update number of squares');
+function createNewGrid(squares) {
+    for (i = 0; i < (Math.pow(squares, 2)); i++) {
+        let div = document.createElement('div');
+        div.classList.add('square');
 
-// function updateGrid(numberOfSquares) {
-//     let numberOfSquares = prompt('Enter the number of squares per side (100 max):');
-// };
+        let squareBorderLeft = document.styleSheets[0].cssRules[0].style;
+        squareBorderLeft.border = '.1px solid black';
 
-btn.addEventListener('click', updateGrid());
+        let squareBorderTop = document.styleSheets[0].cssRules[0].style;
+        squareBorderTop.border = '.1px solid black';
+
+        let squaresWidth = (720 / squares) - 2;
+        console.log(squaresWidth);
+
+        let squareClassWidth = document.styleSheets[0].cssRules[0].style;
+        squareClassWidth.setProperty('width', `${squaresWidth}px`);
+
+        let squareClassHeight = document.styleSheets[0].cssRules[0].style;
+        squareClassHeight.setProperty('height', `${squaresWidth}px`);
+
+        mainDiv.appendChild(div);
+        div.addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'blue';
+
+            setTimeout(() => {
+                e.target.style.backgroundColor = '';
+            }, 1000);
+        })
+    };
+}
 
 // let div = document.createElement('div');
 // div.classList.add('square');
